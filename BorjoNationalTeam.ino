200a70695e2684148f7d66bceb044fc518d2437f
David <davidriveracordo@hotmail.com>
2022-08-02 17:12:21 -0500

    lucecitas

--- BorjoNationalTeam.ino
+++ BorjoNationalTeam.ino
@@ -1,18 +1,92 @@
+void task1()
+{
+    // Definición de estados y variable de estado
+    enum class Task1States
+    {
+        INIT,
+        WAIT_TIMEOUT
+    };
+    static Task1States task1State = Task1States::INIT;
+
+    // Definición de variables static (conservan
+    // su valor entre llamadas a task1)
+    static uint32_t lasTime = 0;
+
+    // Constantes
+
+    constexpr uint32_t INTERVAL = 1000;
+    constexpr uint8_t button1Pin = 12;
+    constexpr uint8_t button2Pin = 13;
+    constexpr uint8_t button3Pin = 32;
+    constexpr uint8_t button4Pin = 33;
+    constexpr uint8_t ledRed = 14;
+    constexpr uint8_t ledGreen = 25;
+    constexpr uint8_t ledBlue = 26;
+    constexpr uint8_t ledYellow = 27;
+
+    // MÁQUINA de ESTADOS
+
+    switch (task1State)
+    {
+    case Task1States::INIT:
+    {
+        Serial.begin(115200);
+        pinMode(button1Pin, INPUT_PULLUP);
+        pinMode(button2Pin, INPUT_PULLUP);
+        pinMode(button3Pin, INPUT_PULLUP);
+        pinMode(button4Pin, INPUT_PULLUP);
+        pinMode(ledRed, OUTPUT);
+        pinMode(ledGreen, OUTPUT);
+        pinMode(ledBlue, OUTPUT);
+        pinMode(ledYellow, OUTPUT);
+        lasTime = millis();
+        task1State = Task1States::WAIT_TIMEOUT;
+
+        break;
+    }
+    case Task1States::WAIT_TIMEOUT:
+    {
+        uint8_t btn1State = digitalRead(button1Pin);
+        uint8_t btn2State = digitalRead(button2Pin);
+        uint8_t btn3State = digitalRead(button3Pin);
+        uint8_t btn4State = digitalRead(button4Pin);
+        uint32_t currentTime = millis();
+
+        // Evento 1:
+        if ((currentTime - lasTime) >= INTERVAL)
+        {
+            lasTime = currentTime;
+            printf("btn1: %d,btn2: %d, btn3: %d, btn4: %d\n", btn1State, btn2State, btn3State, btn4State);
+        }
+
+        // Evento 2
+        if (btn1State == LOW)
+            digitalWrite(ledRed, HIGH);
+        // Evento 3
+        if (btn2State == LOW)
+            digitalWrite(ledGreen, HIGH);
+        // Evento 4
+        if (btn3State == LOW)
+            digitalWrite(ledBlue, HIGH);
+        // Evento 5
+        if (btn4State == LOW)
+            digitalWrite(ledYellow, HIGH);
+
+        break;
+    }
+    default:
+    {
+        Serial.println("Error");
+    }
+    }
+}
+
 void setup()
 {
-  Serial.begin(115200);
+    task1();
 }
 
 void loop()
 {
-    static uint32_t counter = 0;
-    static uint32_t lasTime = 0;
-    static const uint32_t INTERVAL = 1000;
-
-    uint32_t currentTime = millis();
-
-    if( (currentTime - lasTime) >= INTERVAL){
-        lasTime = currentTime;
-      Serial.println(counter++);
-    }
+    task1();
 }
